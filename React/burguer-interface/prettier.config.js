14912799d8dc0734dca13049866494e94a59a4be
MaiaAndrey31 <maiaandrey31@outlook.com>
2023-06-27 11:22:17 -0300

    new commit

--- React/burguer-interface/prettier.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    singleQuote : true,
-    semi :false,
-    arrowParens: 'avoid',
-    trailingComma: 'none',
-
-}
--- React/burguer-interface/src/index.js
+++ React/burguer-interface/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Login from './Containers/Login';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Login from './Containers/Login'
+import GlobalStyles from './Styles/GlobalStyles'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
- <Login />
-);
-
-
+  <>
+    <Login />
+    <GlobalStyles/>
+  </>
+)
