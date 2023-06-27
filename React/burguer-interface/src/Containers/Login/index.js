c3935976195aae5484a24645c07d8bde4a7cce1e
MaiaAndrey31 <maiaandrey31@outlook.com>
2023-06-27 11:55:25 -0300

    new commit

--- React/burguer-interface/src/Containers/Login/index.js
+++ React/burguer-interface/src/Containers/Login/index.js
@@ -1,14 +1,38 @@
-import React from "react"
+import React from 'react'
+
+import {
+  Container,
+  Background,
+  ContainerItems,
+ 
+  Input,
+  CadastroLink,
+  Button,
+  Label,
+} from './style'
 
 function Login() {
-    return (
-        <div>
+  return (
+    <Container>
+      <Background />
+      <ContainerItems>
+        <img />
         <h1>Login</h1>
 
+        <Label>Email</Label>
+        <Input />
 
-        
-        </div>
-    )
+        <Label>Password</Label>
+        <Input />
+
+        <Button>Entrar</Button>
+
+        <CadastroLink>
+          Ainda não tem cadastro?<a>SignUp</a>
+        </CadastroLink>
+      </ContainerItems>
+    </Container>
+  )
 }
 
-export default Login 
+export default Login
---
+++ React/burguer-interface/src/Containers/Login/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+
+`
+export const Background = styled.img`
+
+`
+export const ContainerItems = styled.div`
+
+`
+export const Label = styled.p`
+
+`
+export const Input = styled.input`
+
+`
+export const Button = styled.button`
+
+`
+export const CadastroLink = styled.p`
+
+`
