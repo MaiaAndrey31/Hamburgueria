179f33b6de715477beadb834409ff2e95bdcbcc6
MaiaAndrey31 <maiaandrey31@outlook.com>
2023-07-04 13:16:39 -0300

    new commit

--- React/burguer-interface/src/index.js
+++ React/burguer-interface/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import Login from './Containers/Login'
 import Register from './Containers/Register'
 import GlobalStyles from './Styles/GlobalStyles'
+import { ToastContainer } from 'react-toastify'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>
-    <Login />
+    <Login /> <ToastContainer />
     <GlobalStyles/>
   </>
 )
