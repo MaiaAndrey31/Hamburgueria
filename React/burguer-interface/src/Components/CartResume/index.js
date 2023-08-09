2dc015693b2db7b2d7a2fa271483d06724a32cd5
MaiaAndrey31 <maiaandrey31@outlook.com>
2023-08-09 11:53:10 -0300

    new commit

--- React/burguer-interface/src/Components/CartResume/index.js
+++ React/burguer-interface/src/Components/CartResume/index.js
@@ -1,11 +1,25 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import { useCart } from "../../hooks/CartContext"
 
 import { Container } from "./style"
 import { Button } from "../Button"
+import formatCurrency from "../../utils/FormatCurrency"
 
 
 export function CartResume(){
+    const [finalPrice, setFinalPrice] = useState(0)
+    const [tax] = useState(5)
 
+    const { cartProducts} = useCart()
+
+    useEffect(() => {
+
+        const sumAllItens = cartProducts.reduce((acc, current) => {
+            return current.price * current.quantity + acc
+        }, 0)
+
+        setFinalPrice(sumAllItens)
+    }, [cartProducts])
 
     return (
         <div>
@@ -13,13 +27,13 @@ export function CartResume(){
           <div className="container-top">
             <h2 className="title">Resumo do pedido</h2>
             <p className="itens">Itens</p>
-            <p className="itens-price">R$ 10,00</p>
+            <p className="itens-price">{formatCurrency(finalPrice)}</p>
             <p className="tax">Taxa de entrega</p>
-            <p className="tax-price">R$ 10,00</p>
+            <p className="tax-price">{formatCurrency(tax)}</p>
           </div>
           <div className="container-bottom">
           <p>Total</p>
-          <p>R$ 20,00</p>
+          <p>{formatCurrency(finalPrice + tax)}</p>
           </div>
           
         </Container>
